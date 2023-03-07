bd144ab6c0bd9e8737fcccfc94788bfc16ef27de
malsblack <sebastian.martinez.lopez98@gmail.com>
2023-03-07 15:28:49 -0600

    primer commit

---
+++ Practicas/Suma.java
@@ -0,0 +1,13 @@
+
+public class Suma{
+    public static void main(String args[]){
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        c = a + b;
+        System.out.println("El valor de la suma es :" + c);
+
+
+
+    }
+}
